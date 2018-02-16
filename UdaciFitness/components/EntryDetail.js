450be13bf95b028985fa8bc1d8cdee329b660762
Edgar Zapeka <edz@Edgars-MacBook-Pro.local>
2018-02-15 18:14:44 -0800

    Added menu bar. Some new views

---
+++ UdaciFitness/components/EntryDetail.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import MetricsCard from './MetricsCard'
+import { white } from '../utils/colors'
+
+class EntryDetail extends Component{
+    static navigationOptions = ({ navigation }) => {
+        const {entryId} = navigation.state.params
+
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5,7)
+        const day = entryId.slice(8)
+
+        return {
+            title: `${month}/${day}/${year}`
+        }
+    }
+
+    render(){
+        const {metrics} = this.props
+        return (
+            <View style={styles.container}>
+                <MetricsCard metrics={metrics} />
+                <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15,
+    },
+})
+
+function mapStateToProps(state, { navigation }){
+    const { entryId } = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId]
+    }
+}
+
+export default connect(mapStateToProps)(EntryDetail)
