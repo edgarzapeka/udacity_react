07d39b05234637954274c897568d336051e17063
Edgar Zapeka <edz@Edgars-MacBook-Pro.local>
2018-02-18 17:02:20 -0800

    Added UI for AddDeck componnent

--- learningdecks/components/AddDeck.js
+++ learningdecks/components/AddDeck.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { black, purple } from '../utils/colors'
 
 export default class AddDeck extends Component{
+    state = {
+        deckTitle: ''
+    }
+
     render(){
         return (
             <View style={styles.container}>
-                <Text>Add new deck</Text>
+                <Text style={styles.deckTitle}>Create New Deck</Text>
+                <TextInput
+                    style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({deckTitle: text})}
+                    value={this.state.text}
+                />
+                <Button
+                    onPress={() => console.log('Submitted!')}
+                    title='Submit'
+                    color={purple}
+                    accessibilityLabel="Submit button"
+                />
             </View>
         )
     }
@@ -18,4 +34,9 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    deckTitle: {
+        marginTop: 10,
+        marginBottom: 10,
+        fontSize: 24
+    }
   });
--- learningdecks/utils/colors.js
+++ learningdecks/utils/colors.js
@@ -1,2 +1,3 @@
 export const purple = '#292477'
 export const white = '#fff'
+export const black = '#000000'
